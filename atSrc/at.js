412fd0870b67c3b02117af107251204008361a42
Christopher Reay <christopherreay@gmail.com>
2017-05-01 18:21:41 -0400

    Umm. Should have made loads more commits. Added new functionality to make using namespace much less verbose. Also working on the test for traverse"

--- atSrc/at.js
+++ atSrc/at.js
@@ -23,12 +23,9 @@ else
 ( function(window)
   { 
     var AtRoot = function(atStore)
-    { if (window.hasOwnProperty("atRoot") && atRoot != null)
-      { return;
-      }
-      else
-      { atRoot = this;
-      }
+    { var atRoot = this;
+
+      this.regex = {};
 
       // fudamental identifier for implementation
       this.createID = function()
@@ -106,24 +103,53 @@ else
       if (atStore) atRoot.connectAtStore(atStore);
 
 
+      atRoot.regex.startsWithToReturn = /^\s*toReturn\s*=/;
       //namespace functions are convenience. Since @ makes namespaces mean anything and everything, its nice to be able to create and use them easily.
       this.namespace = function(object, address, defaultList)
-      { defaultCounter        = 0;
-        defaultListTestIndex  = defaultList.length -1;
+      { var current  = object;
 
-        var current = object;
-        address = address.split(".");
+        var addressList           = address.split(".");
+        var addressListTestIndex  = addressList.length -1;
+        var addressCounter        = 0;
 
-        for (var wayPoint of address)
+        // by default just fill the namespace with objects
+        if (defaultList == null) defaultList = ["toReturn = {}"];
+        var defaultListTestIndex  = defaultList.length -1;
+        var defaultCounter        = 0;
+
+        // if the address is more than one item long, and the defaultList is one item long, populate the default list with empty objects, up until 
+        //   the leaf address
+        var nonLeafNodes;
+        if ( defaultList[0].startsWith("nonLeafNodes:") )
+        { nonLeafNodes = defaultList[0].replace(/nonLeafNodes:/, "");
+          console.log("nonLeafNodes: \n  ", nonLeafNodes);
+        }
+        else if ( defaultList[0].startsWith("leafNode:") )
+        { defaultList[0] = defaultList[0].replace(/leafNode:/, "");
+          nonLeafNodes = "toReturn = {}";
+        }
+
+
+        for (var wayPoint of addressList)
         { if (!current.hasOwnProperty(wayPoint) )
           { var toReturn;
-            eval(defaultList[defaultCounter]);
+            var toEval;
+            if (addressCounter < addressListTestIndex && nonLeafNodes != null)
+            { toEval = nonLeafNodes;
+            }
+            else
+            { toEval = defaultList[defaultCounter];
+            }
+            if (! atRoot.regex.startsWithToReturn.test(toEval) ) toEval = "toReturn = "+toEval+";";
+            console.log("toEval:", toEval);
+            eval(toEval);
             // toReturn.name = wayPoint;
             current[wayPoint] =  toReturn;
             console.log("current:", current, "key:", wayPoint, "obj:", current[wayPoint]);
           }
           current = current[wayPoint];
 
+          addressCounter ++;
           if (defaultCounter < defaultListTestIndex)
           { defaultCounter ++;
           }
@@ -143,6 +169,50 @@ else
           current = current[wayPoint];
         }
         return true;
+      }
+
+      // functions for graphs
+      this.AtNode = function()
+      { //At Node is the fundamental object of the programming language. Travellers and Branches are also At Nodes. not sure about "contexts". but probabaly them too
+        //  because nodes are connected by branches and travellers, there is no such thing as a "reference" as far as node.js is concerned. There are only explicit
+        //  references to other node id's. This means that this object can be persisted "as is" into the database (well mongo anyway). Some other database might want
+        //  to pull it apart a bit, but the "id" field should be enough for most things
+
+        this.idDict = new atRoot.createID();
+        // the id is indexed in the store
+        this.id     = this.idDict.idString;
+
+        // if (nodeContent)
+        // { extend(this, nodeContent);
+        // }
+      }
+      
+      this.traverse = function(traveller, context)
+      { //function which takes the traveller and the context. this avoids hard coding the name of the codeBlock / program field into the objects
+        setImmediate(
+            function()
+            { var namespace = atRoot.namespace
+              
+              //evaluate the code in the context against the traveller              
+              //  the node has the ability to set a suggested exit. A difference traverse function could ignore it (e.g. visualisation traveller)
+              var toEval = namespace(context, "traveller.codeBlock", ["toReturn = {}", "toReturn = ''"])
+              console.log("traverse.toEval:\n  ", toEval);
+              toEvalFunction = new Function("traveller", "context", toEval);
+              toEvalFunction(traveller, context);
+
+
+              var destination = namespace(traveller, "traveller.suggestedExit", ["toReturn = {}", "toReturn = null"]) || namespace(context, "traveller.exit", ["toReturn = {}", "toReturn = null"]);
+              traveller.traveller.suggestedExit = null;
+
+              if (!destination) 
+              { console.log("End context:", traveller)
+                if (traveller.traveller.callback) setImmediate(function(){traveller.traveller.callback(traveller);});
+              }
+              else 
+                atRoot.traverse(traveller, register(destination) );
+            }
+        );
+        
       }
 
     }
--- test/test.js
+++ test/test.js
@@ -2,6 +2,14 @@ var assert = require("assert");
 
 var app =  require("../app.js");
 
+var AtRoot = require("../atSrc/at.js");
+
+var atRoot = new AtRoot();
+
+console.log(AtRoot);
+console.log(atRoot);
+
+
 var testObject = {};
 
 describe
@@ -57,5 +65,89 @@ describe
         );
       }
     );
+
+    describe
+    ( "test default defaultList",
+      function()
+      { it
+        ( "",
+          function()
+          { atRoot.namespace(testObject, "create.just.objects");
+            result = Object.keys(testObject.create.just.objects).length === 0 && testObject.create.just.objects.constructor === Object;
+            assert.equal(result, true);
+          }
+        );
+      }
+    );
+    describe
+    ( "test nonLeafNodes",
+      function()
+      { it
+        ( "",
+          function()
+          { atRoot.namespace(testObject, "create.using.nonLeafNodes.going.on.forever", ["nonLeafNodes: {'proof':'true'}", "'someFunnyComment'"]);
+            console.log("testObject\n  ", JSON.stringify(testObject));
+            console.log(testObject.create.using.nonLeafNodes.going.on.forever);
+            console.log(testObject.create.using.nonLeafNodes.going.on.proof);
+            console.log(testObject.create.using.nonLeafNodes.going.on.forever == "someFunnyComment" && testObject.create.using.nonLeafNodes.going.on.proof);
+            var result = 
+                (testObject.create.using.nonLeafNodes.going.on.forever == "someFunnyComment") && testObject.create.using.nonLeafNodes.going.on.proof;
+
+            assert.equal(result, "true");
+          }
+        );
+      }
+    );
+    describe
+    ( "test leafNode",
+      function()
+      { it
+        ( "",
+          function()
+          { atRoot.namespace(testObject, "create.using.leafNode.going.on.for.a.while", ["leafNode: 'someFunnyComment'"]);
+            console.log("testObject\n  ", JSON.stringify(testObject));
+            console.log(JSON.stringify(testObject.create.using.leafNode.going.on.for.a));
+            var result = 
+                (testObject.create.using.leafNode.going.on.for.a.while == "someFunnyComment") && JSON.stringify(testObject.create.using.leafNode.going.on.for.a) == `{"while":"someFunnyComment"}` ;
+
+            assert.equal(result, true);
+          }
+        );
+      }
+    );
   }
+);
+
+describe
+( "basic node and traverse functionality (no persistance)",
+  function()
+  { var context   = new atRoot.AtNode();
+    var traveller = new atRoot.AtNode();
+
+    namespace = atRoot.namespace;
+
+    describe
+    ( "create value 'x' = 0",
+      function()
+      { it
+        ( "should result in the traveller having traveller.test.x = 0",
+          function()
+          { namespace( context, "traveller.codeBlock")
+            context.traveller.codeBlock = 
+            ` namespace(traveller, "test.x", ["leafNode: 0"]);
+              console.log("traveller:\\n  ", traveller)\;
+            `;
+            
+            
+            atRoot.traverse(traveller, context);
+            console.log("testResults\n\n\n\n");
+            console.log("traveller:\n  ", traveller);
+            console.log("context:\n  ", context);
+            assert.equal(traveller.test.x, 0);
+          }
+        );
+      }
+    );
+  }
+
 );
