0090e7b01d9ff97ed66e51e65e3f33e2d1345ae3
Christopher Reay <christopherreay@gmail.com>
2017-05-10 17:05:34 -0400

    added super simple read of the db.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req, res);
-  res.render('index', { title: 'Express' });
+
+  var nodeID = req.query.nodeID;
+  var node = req.atStore.findOne({"id":nodeID});
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(node, null, 3));
 });
 
 module.exports = router;
